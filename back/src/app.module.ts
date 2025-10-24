bfedb15594ddc8d26915526dd602ebdd86bcc0f8
BlipTheClip <superpv1268@gmail.com>
2025-10-24 11:27:18 -0300

    Implementación inicial de solicitudes y generación de PDF

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaModule } from 'prisma/prisma.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthorizationRequestsModule } from './authorization-requests/authorization-requests.module';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, AuthorizationRequestsModule],
 })
 export class AppModule {}
