55350064b9797131cc04352dea40a126776a37f8
vrasantiago <vraniisant@gmail.com>
2025-10-24 12:29:35 -0300

    merge(front-back): integración de frontend y backend

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from 'prisma/prisma.module';
+import { AuthorizationRequestsModule } from './authorization-requests/authorization-requests.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, AuthorizationRequestsModule],
   controllers: [AppController],
   providers: [AppService],
 })
